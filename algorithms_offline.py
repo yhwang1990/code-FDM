c6a4e5f35dbab1efc4bbc46d6c2f9b5c61f78cd2
yhwang1990 <wangyanhao990@gmail.com>
2021-06-06 11:49:05 +0300

    Initial commit

--- algorithms_offline.py
+++ algorithms_offline.py
@@ -1,5 +1,7 @@
+import csv
 import sys
 import numpy as np
+import utils
 
 
 class Element:
@@ -9,18 +11,43 @@ class Element:
         self.features = features
 
 
-def GMM(elements, k, sol, dist):
-    dist_array = np.full(len(elements), sys.maxsize)
-    if len(sol) == 0:
-        sol.append(0)
-        for i in range(len(elements)):
-            dist_array[i] = dist(elements[0].features, elements[i].features)
+def GMM(X, k, S, div, dist):
+    dist_array = np.full(len(X), sys.float_info.max)
+    if len(S) == 0:
+        S.append(0)
+        div.append(sys.float_info.max)
+        for i in range(len(X)):
+            dist_array[i] = dist(X[0].features, X[i].features)
     else:
-        for i in range(len(elements)):
-            for j in sol:
-                dist_array[i] = min(dist_array[i], dist(elements[i].features, elements[j].features))
-    while len(sol) < k:
-        min_idx = np.argmin(dist_array)
-        sol.append(min_idx)
-        for i in range(len(elements)):
-            dist_array[i] = min(dist_array[i], dist(elements[i].features, elements[min_idx].features))
+        for i in range(len(S)):
+            div.append(sys.float_info.max)
+        for i in range(len(X)):
+            for j in S:
+                dist_array[i] = min(dist_array[i], dist(X[i].features, X[j].features))
+    while len(S) < k:
+        max_idx = np.argmax(dist_array)
+        max_dist = np.max(dist_array)
+        S.append(max_idx)
+        div.append(max_dist)
+        for i in range(len(X)):
+            dist_array[i] = min(dist_array[i], dist(X[i].features, X[max_idx].features))
+
+
+if __name__ == "__main__":
+    elements = []
+    with open("datasets/blobs_n100_m2.csv") as fileobj:
+        csvreader = csv.reader(fileobj, delimiter=',')
+        for row in csvreader:
+            features = [float(row[2]), float(row[3])]
+            elem = Element(int(row[0]), int(row[1]), features)
+            elements.append(elem)
+    sol = []
+    diversity = []
+    GMM(X=elements, k=10, S=sol, div=diversity, dist=utils.cosine_dist)
+    print(sol)
+    print(diversity[1:])
+
+    solution = []
+    for idx in sol:
+        solution.append(elements[idx].features)
+    print(utils.diversity(solution, utils.cosine_dist))
--- utils.py
+++ utils.py
@@ -22,7 +22,7 @@ def cosine_dist(point1, point2):
 
 
 def diversity(points, dist):
-    div = sys.maxsize
+    div = sys.float_info.max
     for i in range(len(points)):
         for j in range(len(points)):
             if i != j:
