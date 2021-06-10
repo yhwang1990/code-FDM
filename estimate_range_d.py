2406ef80a926062564509500bd8c71cc91966076
yhwang1990 <wangyanhao990@gmail.com>
2021-06-10 13:33:13 +0300

    Initial commit

---
+++ estimate_range_d.py
@@ -0,0 +1,88 @@
+import csv
+import random
+import sys
+
+import algorithms_offline
+import utils
+
+
+# estimate d_max and d_min for Adult
+# elements = []
+# with open("datasets/adult.csv", "r") as fileobj:
+#     csvreader = csv.reader(fileobj, delimiter=',')
+#     for row in csvreader:
+#         features = []
+#         for i in range(4, 10):
+#             features.append(float(row[i]))
+#         elem = utils.Element(int(row[0]), int(row[1]), features)
+#         elements.append(elem)
+# for k in range(5, 51, 5):
+#     div_min = sys.float_info.max
+#     div_max = 0
+#     for run in range(10):
+#         random.Random(run).shuffle(elements)
+#         for new_idx in range(len(elements)):
+#             elements[new_idx].idx = new_idx
+#         solution, value = algorithms_offline.GMM(X=elements, k=k, init=[], dist=utils.euclidean_dist)
+#         print(solution, value[-1])
+#         div_min = min(div_min, value[-1])
+#         div_max = max(div_max, value[-1])
+#         for c in range(2):
+#             solution, value = algorithms_offline.GMMC(X=elements, color=c, k=k, init=[], dist=utils.euclidean_dist)
+#             print(solution, value[-1])
+#             div_min = min(div_min, value[-1])
+#             div_max = max(div_max, value[-1])
+#     print(div_min, div_max)
+#     d_min = div_min * 0.75
+#     d_max = div_max * 1.25
+#     print(k, d_min, d_max)
+
+# elements.clear()
+# with open("datasets/adult.csv", "r") as fileobj:
+#     csvreader = csv.reader(fileobj, delimiter=',')
+#     for row in csvreader:
+#         features = []
+#         for i in range(4, 10):
+#             features.append(float(row[i]))
+#         elem = utils.Element(int(row[0]), int(row[2]), features)
+#         elements.append(elem)
+# for k in range(5, 51, 5):
+#     div_min = sys.float_info.max
+#     div_max = 0
+#     for run in range(10):
+#         random.Random(run).shuffle(elements)
+#         for new_idx in range(len(elements)):
+#             elements[new_idx].idx = new_idx
+#         for c in range(5):
+#             solution, value = algorithms_offline.GMMC(X=elements, color=c, k=k, init=[], dist=utils.euclidean_dist)
+#             print(solution, value[-1])
+#             div_min = min(div_min, value[-1])
+#             div_max = max(div_max, value[-1])
+#     print(div_min, div_max)
+#     d_min = div_min * 0.75
+#     d_max = div_max * 1.25
+#     print(k, d_min, d_max)
+
+# elements.clear()
+# with open("datasets/adult.csv", "r") as fileobj:
+#     csvreader = csv.reader(fileobj, delimiter=',')
+#     for row in csvreader:
+#         features = []
+#         for i in range(4, 10):
+#             features.append(float(row[i]))
+#         elem = utils.Element(int(row[0]), int(row[3]), features)
+#         elements.append(elem)
+# for k in range(10, 51, 5):
+#     div_min = sys.float_info.max
+#     div_max = 0
+#     for run in range(10):
+#         random.Random(run).shuffle(elements)
+#         for c in range(10):
+#             solution, value = algorithms_offline.GMMC(X=elements, color=c, k=k, init=[], dist=utils.euclidean_dist)
+#             print(solution, value[-1])
+#             div_min = min(div_min, value[-1])
+#             div_max = max(div_max, value[-1])
+#     print(div_min, div_max)
+#     d_min = div_min * 0.75
+#     d_max = div_max * 1.25
+#     print(k, d_min, d_max)
--- utils.py
+++ utils.py
@@ -1,11 +1,18 @@
 import sys
 import numpy as np
 from scipy.spatial import distance
-from typing import Any, Callable, List
+from typing import Any, Callable, Dict, List
 
 
 class Element:
     def __init__(self, idx: int, color: int, features: List[float]):
+        self.idx = idx
+        self.color = color
+        self.features = features
+
+
+class ElementSparse:
+    def __init__(self, idx: int, color: int, features: Dict):
         self.idx = idx
         self.color = color
         self.features = features
@@ -17,16 +24,56 @@ def euclidean_dist(elem1: Element, elem2: Element) -> float:
     return distance.euclidean(elem1.features, elem2.features)
 
 
+def euclidean_dist_sparse(elem1: ElementSparse, elem2: ElementSparse) -> float:
+    sum_len = 0.0
+    for j in elem1.features.keys():
+        if j in elem2.features.keys():
+            sum_len += (elem1.features[j] - elem2.features[j]) * (elem1.features[j] - elem2.features[j])
+        else:
+            sum_len += elem1.features[j] * elem1.features[j]
+    for j in elem2.features.keys():
+        if j not in elem2.features.keys():
+            sum_len += elem2.features[j] * elem2.features[j]
+    return np.sqrt(sum_len)
+
+
 def manhattan_dist(elem1: Element, elem2: Element) -> float:
     if len(elem1.features) != len(elem2.features):
         raise AssertionError("dimension not match")
     return distance.cityblock(elem1.features, elem2.features)
 
 
+def manhattan_dist_sparse(elem1: ElementSparse, elem2: ElementSparse) -> float:
+    sum_len = 0.0
+    for j in elem1.features.keys():
+        if j in elem2.features.keys():
+            sum_len += np.abs(elem1.features[j] - elem2.features[j])
+        else:
+            sum_len += elem1.features[j]
+    for j in elem2.features.keys():
+        if j not in elem2.features.keys():
+            sum_len += elem2.features[j]
+    return sum_len
+
+
 def cosine_dist(elem1: Element, elem2: Element) -> float:
     if len(elem1.features) != len(elem2.features):
         raise AssertionError("dimension not match")
     return np.arccos(1.0 - distance.cosine(elem1.features, elem2.features))
+
+
+def cosine_dist_sparse(elem1: ElementSparse, elem2: ElementSparse) -> float:
+    inner_prod = 0.0
+    length1 = 0.0
+    length2 = 0.0
+    for j in elem1.features.keys():
+        if j in elem2.features.keys():
+            inner_prod += elem1.features[j] * elem2.features[j]
+        length1 += elem1.features[j] * elem1.features[j]
+    for j in elem2.features.keys():
+        length2 += elem2.features[j] * elem2.features[j]
+    cosine = inner_prod / (np.sqrt(length1) * np.sqrt(length2))
+    return np.arccos(cosine)
 
 
 def diversity(elements: List[Element], dist: Callable[[Any, Any], float]) -> float:
@@ -36,3 +83,15 @@ def diversity(elements: List[Element], dist: Callable[[Any, Any], float]) -> flo
             if i != j:
                 div = min(div, dist(elements[i], elements[j]))
     return div
+
+
+# if __name__ == "__main__":
+#     a = ElementSparse(0, 0, {0: 1.0, 1: 1.0})
+#     b = ElementSparse(0, 0, {0: 1.0, 2: 1.0})
+#     c = ElementSparse(0, 0, {1: 1.0, 3: 1.0})
+#     print(euclidean_dist_sparse(a, b))
+#     print(manhattan_dist_sparse(a, b))
+#     print(cosine_dist_sparse(a, b))
+#     print(euclidean_dist_sparse(c, b))
+#     print(manhattan_dist_sparse(c, b))
+#     print(cosine_dist_sparse(c, b))
