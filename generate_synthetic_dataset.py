8a4a8f808e270b85ea25def93212906193a65251
yhwang1990 <wangyanhao990@gmail.com>
2021-06-13 09:46:07 +0300

    Initial commit

--- generate_synthetic_dataset.py
+++ generate_synthetic_dataset.py
@@ -2,19 +2,17 @@ import argparse
 import numpy as np
 from sklearn.datasets import make_blobs
 
-global args
 
-
-def generate_blobs():
-    X, y = make_blobs(n_samples=args.size, centers=10, n_features=args.dim, random_state=17)
-    g = np.random.randint(args.group, size=args.size, dtype=np.uint8)
-    with open("datasets/blobs_n" + str(args.size) + "_m" + str(args.group) + ".csv", "a") as fileobj:
-        for i in range(args.size):
+def generate_blobs(size: int, group: int, dim: int) -> None:
+    X, y = make_blobs(n_samples=size, centers=10, n_features=dim, random_state=17)
+    g = np.random.randint(group, size=size, dtype=np.uint8)
+    with open("datasets/blobs_n" + str(size) + "_m" + str(group) + ".csv", "a") as fileobj:
+        for i in range(size):
             fileobj.write(str(i) + ",")
             fileobj.write(str(g[i]) + ",")
-            for j in range(args.dim - 1):
+            for j in range(dim - 1):
                 fileobj.write(format(X[i][j], ".6f") + ",")
-            fileobj.write(format(X[i][args.dim - 1], ".6f") + "\n")
+            fileobj.write(format(X[i][dim - 1], ".6f") + "\n")
 
 
 if __name__ == "__main__":
@@ -23,4 +21,4 @@ if __name__ == "__main__":
     parser.add_argument("-m", "--group", default=2, type=int)
     parser.add_argument("-d", "--dim", default=2, type=int)
     args = parser.parse_args()
-    generate_blobs()
+    generate_blobs(args.size, args.group, args.dim)
